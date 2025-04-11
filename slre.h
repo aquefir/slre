b2900dd8607e41db990a0c3581267a864524a52d
Alexander Nicholi <me@nicholatian.com>
2025-04-11 01:17:40 -0400

    reformat all files

--- slre.h
+++ slre.h
@@ -1,60 +1,66 @@
 /*
- * Copyright (c) 2004-2013 Sergey Lyubka <valenok@gmail.com>
- * Copyright (c) 2013 Cesanta Software Limited
- * All rights reserved
+ * Copyright (C) 2004-2013 Sergey Lyubka <valenok@gmail.com>
+ * Copyright (C) 2013 Cesanta Software Limited
+ * Copyright (C) 2025 Aquefir Consulting LLC <//aquefir.co/>
+ * All rights reserved.
  *
- * This library is dual-licensed: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation. For the terms of this
- * license, see <http://www.gnu.org/licenses/>.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2.
  *
- * You are free to use this library under the terms of the GNU General
- * Public License, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
- * Alternatively, you can license this library under a commercial
- * license, as set out in <http://cesanta.com/products.html>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
  */
 
 /*
- * This is a regular expression library that implements a subset of Perl RE.
- * Please refer to README.md for a detailed reference.
+ * This is a regular expression library that implements a subset of Perl
+ * RE. Please refer to README.md for a detailed reference.
  */
 
-#ifndef CS_SLRE_SLRE_H_
-#define CS_SLRE_SLRE_H_
+#ifndef INC_API__SLRE_H
+#define INC_API__SLRE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-struct slre_cap {
-  const char *ptr;
-  int len;
-};
-
-
-int slre_match(const char *regexp, const char *buf, int buf_len,
-               struct slre_cap *caps, int num_caps, int flags);
-
-/* Possible flags for slre_match() */
-enum { SLRE_IGNORE_CASE = 1 };
-
+#if !defined( __cplusplus )
+#define SLRE_API extern
+#else
+#define SLRE_API extern "C"
+#endif /* !defined( __cplusplus ) */
 
 /* slre_match() failure codes */
-#define SLRE_NO_MATCH               -1
-#define SLRE_UNEXPECTED_QUANTIFIER  -2
-#define SLRE_UNBALANCED_BRACKETS    -3
-#define SLRE_INTERNAL_ERROR         -4
-#define SLRE_INVALID_CHARACTER_SET  -5
-#define SLRE_INVALID_METACHARACTER  -6
-#define SLRE_CAPS_ARRAY_TOO_SMALL   -7
-#define SLRE_TOO_MANY_BRANCHES      -8
-#define SLRE_TOO_MANY_BRACKETS      -9
+#define SLRE_NO_MATCH ( -1 )
+#define SLRE_UNEXPECTED_QUANTIFIER ( -2 )
+#define SLRE_UNBALANCED_BRACKETS ( -3 )
+#define SLRE_INTERNAL_ERROR ( -4 )
+#define SLRE_INVALID_CHARACTER_SET ( -5 )
+#define SLRE_INVALID_METACHARACTER ( -6 )
+#define SLRE_CAPS_ARRAY_TOO_SMALL ( -7 )
+#define SLRE_TOO_MANY_BRANCHES ( -8 )
+#define SLRE_TOO_MANY_BRACKETS ( -9 )
 
-#ifdef __cplusplus
-}
-#endif
+struct slre_cap
+{
+	const char * ptr;
+	int len;
+};
 
-#endif /* CS_SLRE_SLRE_H_ */
+SLRE_API int slre_match( const char * regexp,
+	const char * buf,
+	int buf_len,
+	struct slre_cap * caps,
+	int num_caps,
+	int flags );
+
+/* Possible flags for slre_match() */
+enum
+{
+	SLRE_IGNORE_CASE = 1
+};
+
+#endif /* INC_API__SLRE_H */
